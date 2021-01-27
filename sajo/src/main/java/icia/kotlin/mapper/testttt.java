901d72e733398c7b35f93f174058814131fed67f
go804 <go804@183.109.19.50>
2021-01-27 13:42:39 +0900

    2021-01-27 13:42 test

---
+++ sajo/src/main/java/icia/kotlin/mapper/testttt.java
@@ -0,0 +1,11 @@
+package icia.kotlin.mapper;
+
+public class testttt {
+
+	public void testerrrrrr() {
+		
+		System.out.println("test 성공!");
+		
+	}
+	
+}
---
+++ sajo/src/main/java/icia/kotlin/sajo/Changes.java
@@ -0,0 +1,5 @@
+package icia.kotlin.sajo;
+
+public class Changes {
+
+}
